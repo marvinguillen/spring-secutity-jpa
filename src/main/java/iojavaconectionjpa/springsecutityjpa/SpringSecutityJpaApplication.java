14ab06a2d2d78651932e1184e3521d3d31abd176
marvin.a.guillen@gmail.com <marvin.a.guillen@gmail.com>
2021-01-12 18:50:24 -0600

    First commited to my project jpa

---
+++ src/main/java/iojavaconectionjpa/springsecutityjpa/SpringSecutityJpaApplication.java
@@ -0,0 +1,13 @@
+package iojavaconectionjpa.springsecutityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecutityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecutityJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/iojavaconectionjpa/springsecutityjpa/SpringSecutityJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package iojavaconectionjpa.springsecutityjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecutityJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
